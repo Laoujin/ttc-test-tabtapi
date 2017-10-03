16754e7825dbfa72cb14235d9ebd7cc907aef750
Laoujin <woutervs@hotmail.com>
2017-10-04 00:47:59 +0200

    sporta new wsdl

--- settings.ini.php
+++ settings.ini.php
@@ -4,7 +4,7 @@ Password =
 
 ; WSDL Urls
 VTTL = http://api.vttl.be/0.7/?wsdl
-Sporta = http://tafeltennis.sporcrea.be/api/?wsdl
+Sporta = http://ttonline.sporta.be/api/?wsdl
 KAVVV = http://tafeltennis.kavvv.be/api/?wsdl
 
 ; Defaults
